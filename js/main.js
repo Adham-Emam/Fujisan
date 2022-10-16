c52d4ad9d7ba3c68c3fa0837280838519904a8b9
Adham_Emam <mrado372002@gmail.com>
2022-10-16 15:23:17 +0200

    upload

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ let imgs = ["bg1.webp", "bg2.webp", "bg3.webp", "bg4.webp", "bg5.jpg"];
 
 let element = document.getElementById("landing");
 
-function change() {
+const change = () => {
   let randBg = Math.floor(Math.random() * imgs.length);
   element.style.backgroundImage = `url(./images/${imgs[randBg]})`;
-}
+};
 setInterval(change, 2000);
 
 let resUl = document.querySelector(".res-ul");
@@ -27,4 +27,34 @@ settingbtn.onclick = function () {
   settingIcon.classList.toggle("fa-spin");
   setting.classList.toggle("setting-active");
   resUl.classList.remove("res-ul-active");
+};
+
+let first = document.querySelector(".first");
+let second = document.querySelector(".second");
+let third = document.querySelector(".third");
+let fourth = document.querySelector(".fourth");
+
+first.onclick = function () {
+  document.documentElement.style.setProperty("--main-color", "#16c2b1");
+};
+second.onclick = function () {
+  document.documentElement.style.setProperty("--main-color", "#f9aa33");
+};
+third.onclick = function () {
+  document.documentElement.style.setProperty("--main-color", "#6200ee");
+};
+fourth.onclick = function () {
+  document.documentElement.style.setProperty("--main-color", "#c51162");
+};
+
+let on = document.querySelector(".on");
+let off = document.querySelector(".off");
+
+on.onclick = function () {
+  on.classList.add("ani-active");
+  off.classList.remove("ani-active");
+};
+off.onclick = function () {
+  off.classList.add("ani-active");
+  on.classList.remove("ani-active");
 };
